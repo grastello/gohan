d72e330050b8ae20ac34b4fe79ee82398a61f34e
grastello <gabriele.rastello@gmail.com>
2025-02-14 08:36:07 +0100

    Replace ido with the counsel-ivy-swiper combo

--- emacs/.emacs.d/qol-and-aesthetics.el
+++ emacs/.emacs.d/qol-and-aesthetics.el
@@ -62,25 +62,47 @@
 (electric-pair-mode t)
 
 ;; IDO + SMEX for nice completion in minibuffer
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
+;; (setq ido-enable-flex-matching t)
+;; (setq ido-everywhere t)
+;; (ido-mode 1)
 
-(use-package ido-completing-read+
-  :ensure t
-  :config
-  (ido-ubiquitous-mode 1))
+;; (use-package ido-completing-read+
+;;   :ensure t
+;;   :config
+;;   (ido-ubiquitous-mode 1))
 
-(use-package ido-vertical-mode
-  :ensure t
-  :config
-  (ido-vertical-mode 1)
-  (setq ido-vertical-define-keys 'C-n-and-C-p-only))
+;; (use-package ido-vertical-mode
+;;   :ensure t
+;;   :config
+;;   (ido-vertical-mode 1)
+;;   (setq ido-vertical-define-keys 'C-n-and-C-p-only))
 
-(use-package smex
+;; (use-package smex
+;;   :ensure t
+;;   :config
+;;   (global-set-key (kbd "M-x") 'smex))
+
+;; Ivy, counsel, swiper
+(use-package ivy
   :ensure t
+  :init
+  (ivy-mode))
+
+(use-package counsel
+  :ensure t
+
   :config
-  (global-set-key (kbd "M-x") 'smex))
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  
+  :init
+  (counsel-mode))
+
+(use-package swiper
+  :ensure t
+
+  :bind
+  (("C-s" . 'swiper)))
 
 ;; which-key
 (use-package which-key
