a13206d6ddda633e4a1b34bacf92f76bb1e0e4ac
grastello <gabriele.rastello@gmail.com>
2025-02-15 23:18:05 +0100

    Clean whitespace

--- emacs/.emacs.d/qol-and-aesthetics.el
+++ emacs/.emacs.d/qol-and-aesthetics.el
@@ -73,7 +73,7 @@
   :config
   (setq ivy-use-virtual-buffers t)
   (setq enable-recursive-minibuffers t)
-
+  
   :init
   (counsel-mode))
 
@@ -104,6 +104,6 @@
   :ensure t
   :config
   (setq  zoom-size  '(0.618 . 0.618))
-
+  
   :init
   (zoom-mode))
